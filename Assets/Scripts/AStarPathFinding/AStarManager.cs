3107fe5f7dc1df876343a4d5d0abe91583e128d2
AquaPhoenix07 <aquaphoenix040707@gmail.com>
2026-05-08 21:40:58 +0700

    Tạm hoàn thành AI cho animal

--- Assets/Scripts/AStarPathFinding/AStarManager.cs
+++ Assets/Scripts/AStarPathFinding/AStarManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
+using UnityEditor.TerrainTools;
 
 public class AStarManager : MonoBehaviour
 {
@@ -23,17 +24,48 @@ public class AStarManager : MonoBehaviour
         PathNode startNode = new PathNode(startPos);
         PathNode targetNode = new PathNode(targetPos);
         
-        List<PathNode> resultPath = new List<PathNode>();
-        List<PathNode> frontierList = new List<PathNode{startNode};
+        List<PathNode> frontierList = new List<PathNode> {startNode};
         List<PathNode> closedList = new List<PathNode>();
 
         while (frontierList.Count > 0)
         {
             PathNode currentNode = GetBestNodeFrontier(frontierList);
+            
+            // 1. Kiểm tra nếu đã chạm đích
+            if (Vector3.Distance(currentNode.position, targetPos) < 0.1f)
+            {
+                return RetracePath(startNode, currentNode);
+            }
             frontierList.Remove(currentNode);
             closedList.Add(currentNode);
             
+            //2.Xét các ô láng giêng
+            foreach (Vector3 direction in new Vector3[] { Vector3.up, Vector3.down, Vector3.right, Vector3.left })
+            {
+                Vector3 neighborPos = currentNode.position + direction;
+                
+                // Kiểm tra nếu là vật cản hoặc đã check rồi ( nằm trong closedList)
+                if (!IsWalkable(neighborPos) || IsPositionInList(neighborPos, closedList)) continue;
+               
+                int newGCost = currentNode.GCost + 10;
+                PathNode neighborNode = GetNodeInList(neighborPos, frontierList);
+                
+                // Nếu ô láng giềng này mới tinh hoặc tìm được đường tới rẻ hơn 
+                if (neighborNode == null || newGCost < neighborNode.GCost)
+                {
+                    if (neighborNode == null)
+                    {
+                        neighborNode = new PathNode(neighborPos);
+                        frontierList.Add(neighborNode);
+                    }
+
+                    neighborNode.GCost = newGCost;
+                    neighborNode.HCost = (int)GetDistance(neighborNode, targetNode);
+                    neighborNode.previousNode = currentNode;
+                }
+            }
         }
+        return null;
     }
 
     public PathNode GetBestNodeFrontier(List<PathNode> pathNodeList)
@@ -55,28 +87,53 @@ public class AStarManager : MonoBehaviour
         }
         return lowestFCostNode;
     }
-
-    private List<PathNode> GetNeighborsNode(PathNode currentNode)
-    {
-        foreach (Vector3 direction in new Vector3[] { Vector3.up, Vector3.down, Vector3.right, Vector3.left })
-        {
-            Vector3 neighborPos = currentNode.position + direction;
-            if (!IsWalkable(neighborPos) || closedList.Any(n => Vec)) 
-
-        }
-    }
-
+    
     private bool IsWalkable(Vector3 checkNode)
     {
         Collider2D hit  = Physics2D.OverlapPoint(checkNode, obstacleLayer);
         return hit == null;
     }
 
-    private float GetDistane(PathNode A, PathNode B)
+    private float GetDistance(PathNode A, PathNode B)
     {
         float xDistance = Mathf.Abs(A.position.x - B.position.x);
         float yDistance = Mathf.Abs(A.position.y - B.position.y);
-        return xDistance + yDistance;
+        return (int)(xDistance + yDistance )* 10 ;
+    }
+
+    private List<Vector3> RetracePath(PathNode startNode, PathNode endNode)
+    {
+        List<Vector3> path = new List<Vector3>();
+        PathNode currentNode = endNode;
+        while (currentNode != null)
+        {
+            path.Add(currentNode.position);
+            if (currentNode == startNode) break;
+            currentNode = currentNode.previousNode;
+        }
+        path.Reverse();
+        return path;
+    }
+
+    private bool IsPositionInList(Vector3 pos, List<PathNode> list)
+    {
+        foreach (var node in list)
+        {
+            if (Vector3.Distance(node.position, pos) < 0.1f) return true;
+        }
+        return false;
+    }
+
+    private PathNode GetNodeInList(Vector3 pos, List<PathNode> list)
+    {
+        foreach (var node in list)
+        {
+            if (Vector3.Distance(node.position, pos) < 0.1f)
+            {
+                return node;
+            }
+        }
+        return null;
     }
 
 }
--- Assets/Scripts/AStarPathFinding/AnimalAI.cs
+++ Assets/Scripts/AStarPathFinding/AnimalAI.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class AnimalAI : MonoBehaviour
 {
+    public Transform shelterTarget;
+
+    public float moveDelay = 0.5f;
+
+    private AStarManager aStarManager;
+    private bool isMoving = false;
+    private List<Vector3> resultPath;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        aStarManager = FindObjectOfType<AStarManager>();
+        StartCoroutine(ThinkAndMoveRoutine());
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator ThinkAndMoveRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(moveDelay);
+            if (!isMoving && shelterTarget != null)
+            {
+                if (Vector3.Distance(shelterTarget.position, transform.position) < 0.1f)
+                {
+                    Debug.Log("Về Đích");
+                    yield break;
+                }
+            }
+            
+            resultPath = aStarManager.FindPath(transform.position, shelterTarget.position);
+            if (resultPath != null && resultPath.Count > 0)
+            {
+                Vector3 nextPos = resultPath[0];
+                gameObject.transform.position = nextPos;
+            }
+        }
+        
+
     }
 }
