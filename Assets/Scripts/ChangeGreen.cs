2ef7ac7c325b4e0618f07eae09ae111bbc90ff7e
AquaPhoenix07 <aquaphoenix040707@gmail.com>
2026-05-11 03:26:48 +0700

    Xong hàm ChangeGreen làm đổi các ô cỏ khi chơi xong

---
+++ Assets/Scripts/ChangeGreen.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections;
+using System.Collections.Generic;
+public class ChangeGreen : MonoBehaviour
+{
+    private GameManager gameManager;
+    [Header("Map Change")]
+    public Tilemap groundTilemap;
+    public Tilemap wallTilemap;
+    
+    [Header("TileBase ChangePairs")]
+    public List<TileBase> oldGroundTiles;
+    public List<TileBase> newGroundTiles;
+    
+    public List<TileBase> oldWallTiles;
+    public List<TileBase> newWallTiles;
+
+    [Header("VisualEffect")] [SerializeField]
+    private float spreadSpeed = 0.02f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        StartCoroutine("SpreadGrassGround");
+        StartCoroutine("SpreadGrassWall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+// Thay TileBase cho ground
+    private IEnumerator SpreadGrassGround()
+    {
+        yield return new WaitUntil(() => gameManager.Target == gameManager.currentTarget);
+        
+        List<TileChangeData> tilesToChange = new List<TileChangeData>();
+        BoundsInt bounds = groundTilemap.cellBounds;
+
+        foreach (var pos in bounds.allPositionsWithin)
+        {
+            TileBase currentTile = groundTilemap.GetTile(pos);
+            if (currentTile == null) continue;
+            for (int i = 0; i < oldGroundTiles.Count; i++)
+            {
+                if (currentTile == oldGroundTiles[i])
+                {
+                    tilesToChange.Add(new TileChangeData
+                    {
+                        position = pos,
+                        newTile = newGroundTiles[i]
+                    });
+                    break;
+                }  
+            }
+        }
+
+        foreach (var tile in tilesToChange)
+        {
+            groundTilemap.SetTile(tile.position, tile.newTile);
+            yield return new WaitForSeconds(spreadSpeed);
+        }
+    }
+// Thay TileBase cho Wall 
+    private IEnumerator SpreadGrassWall()
+    {
+        yield return new WaitUntil(() => gameManager.Target == gameManager.currentTarget);
+        
+        List<TileChangeData> tilesToChange = new List<TileChangeData>();
+        BoundsInt bounds = wallTilemap.cellBounds;
+        foreach (var pos in bounds.allPositionsWithin)
+        {
+            TileBase currentTile = wallTilemap.GetTile(pos);
+            if (currentTile == null) continue;
+            for (int i = 0; i < oldWallTiles.Count; i++)
+            {
+                if (currentTile == oldWallTiles[i])
+                {
+                    tilesToChange.Add(new TileChangeData
+                    {
+                        position = pos,
+                        newTile = newWallTiles[i]
+                    });
+                    break;
+                }
+            }
+        }
+
+        foreach (var tile in tilesToChange)
+        {
+            wallTilemap.SetTile(tile.position, tile.newTile);
+            yield return new WaitForSeconds(spreadSpeed);
+        }
+    }
+
+    private class TileChangeData
+    {
+        public Vector3Int position;
+        public TileBase newTile;
+    }
+
+    
+}
