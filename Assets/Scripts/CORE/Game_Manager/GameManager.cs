0b9797ecf6cca98cb586f63285d2ad956dad5c53
AquaPhoenix07 <aquaphoenix040707@gmail.com>
2026-06-05 21:14:43 +0700

    Nâng cấp thành công file code GameManager

--- Assets/Scripts/CORE/Game_Manager/GameManager.cs
+++ Assets/Scripts/CORE/Game_Manager/GameManager.cs
@@ -1,160 +1,65 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using System.Linq;
+
+[System.Serializable]
+// Cho phép mở khoá các class và chỉnh sửa thông tin trong nó
+public class MapElement
+{
+    public string elementName;
+    public TileBase tileToScan;
+    public GameObject prefabToSpawn;
+    public bool isTarget;
+    public bool isTallObject;
+    public TileBase tallTile;
+}
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Map Settings")] // Tạo ra một header trong bảng Inspector
-    public Tilemap map;
-    //Box Tile
-    [Header("Box Tile")]
-    //LEFT
-    public TileBase garbageTile;
-    public GameObject garbagePrefab;
-    public TileBase fertilizerTile;
-    public GameObject fertilizerPrefab;
-    //RIGHT
-    public TileBase freshWaterTile;
-    public GameObject freshWaterPrefab;
-    public TileBase magicSeedTile;
-    public GameObject magicSeedPrefab;
-    
-    //Target Tile
-    [Header("Target")]
-    //LEFT
-    public TileBase trashcanTile;
-    public GameObject emptyTrashcanPrefab;
-    
-    public TileBase trunkTile;
-    public GameObject trunkPrefab;
-    //RIGHT
-    public TileBase holeToPlantTile;
-    public GameObject holeToPlantPrefab;
+    [Header("Map Setting")] public Tilemap map;
 
-    public TileBase posionLakeTile;
-    public GameObject posionLakePrefab;
-    
-    // Shelter Tile
-    [Header("Shelter")]
-    public TileBase caveTile;
-    public GameObject cavePrefab;
+    [Header("Map Element Configuration")] public List<MapElement> mapElements;
 
-    public TileBase treeHouseTile;
-    public GameObject treeHousePrefab;
-    //Target
     public int Target = 0;
     public int currentTarget = 0;
     public bool isMapReady;
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         ScanMap();
         isMapReady = true;
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     private void ScanMap()
     {
-        // Lấy kích thước bao trùm map
         BoundsInt bounds = map.cellBounds;
-        
-        // Trả về Vector3Int là toạ độ số thứ tự ô trong Cell,
-        // Ví dụ: "Ô ở cột số 5, hàng số 2". (Chỉ số: 5, 2, 0).
-        foreach (var pos in bounds.allPositionsWithin) 
+        foreach (var pos in bounds.allPositionsWithin)
         {
             TileBase currentTile = map.GetTile(pos);
+            if (currentTile == null) continue;
             
-            
-            //Đổi tile thành GameObject cho Items
-            if (currentTile == garbageTile)
-            { 
-                // Trả về toạ độ gốc của ô trong world map từ toạ độ số thứ tự ô trong Cell
-                Vector3 currentPos = map.CellToWorld(pos);
-                
-                Instantiate(garbagePrefab, currentPos, garbagePrefab.transform.rotation);
-                // Set viên gạch tại vị trí cũ biến mất cho thùng đứng
-                map.SetTile(pos, null);
-            }
-            else if (currentTile == fertilizerTile)
+            MapElement matchedElement = mapElements.FirstOrDefault(x => x.tileToScan == currentTile);
+            if (matchedElement != null)
             {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(fertilizerPrefab, currentPos, fertilizerPrefab.transform.rotation);
-                map.SetTile(pos, null);
-            }
-            else if (currentTile == freshWaterTile)
-            {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(freshWaterPrefab, currentPos, freshWaterPrefab.transform.rotation);
-                map.SetTile(pos, null);
-            }
-            else if (currentTile == magicSeedTile)
-            {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(magicSeedPrefab, currentPos, magicSeedPrefab.transform.rotation);
+                Vector3 currentPosition = map.CellToWorld(pos);
+                Instantiate(matchedElement.prefabToSpawn, currentPosition, matchedElement.prefabToSpawn.transform.rotation);
+
+                if (matchedElement.isTarget)
+                {
+                    Target++;
+                }
                 map.SetTile(pos,null);
+                if (matchedElement.isTallObject)
+                {
+                    Vector3 upperPos = currentPosition + new Vector3(0, 2, 0);
+                    Vector3Int upperTile = map.WorldToCell(upperPos);
+                    map.SetTile(upperTile, matchedElement.tallTile);
+                }
             }
-            
-            
-            // Đổi Tile thành GameObject cho Target
-            else if (currentTile == trashcanTile)
-            {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(emptyTrashcanPrefab, currentPos, emptyTrashcanPrefab.transform.rotation);
-                Target++;
-                map.SetTile(pos, null);
-            }
-            else if (currentTile == trunkTile)
-            {
-                Vector3 upperPos = new Vector3(pos.x, pos.y + 1, 0);
-                
-                Vector3Int upperTile = map.WorldToCell(upperPos);
-                Vector3 currentPos = map.CellToWorld(pos);
-                
-                Instantiate(trunkPrefab, currentPos, trunkPrefab.transform.rotation);
-                Target++; 
-                
-                map.SetTile(pos,null);
-                map.SetTile(upperTile, null);
-            }
-            else if (currentTile == posionLakeTile)
-            {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(posionLakePrefab, currentPos, posionLakePrefab.transform.rotation);
-                Target++;
-                map.SetTile(pos,null);
-            }
-            else if (currentTile == holeToPlantTile)
-            {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(holeToPlantPrefab, currentPos, holeToPlantPrefab.transform.rotation);
-                Target++;
-                map.SetTile(pos,null);
-            }
-            
-            
-            // Đổi Tile thành GameObject cho Shelter
-            else if (currentTile == caveTile)
-            {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(cavePrefab, currentPos, cavePrefab.transform.rotation);
-                map.SetTile(pos,null);  
-            }
-            else if (currentTile == treeHouseTile)
-            {
-                Vector3 currentPos = map.CellToWorld(pos);
-                Instantiate(treeHousePrefab, currentPos, treeHousePrefab.transform.rotation);
-                map.SetTile(pos,null);
-            }
+
         }
     }
-    
     private void CheckTarget()
     {
         Debug.Log($"[GameManager] Điểm hiện tại: {currentTarget} / {Target}");
